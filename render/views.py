0abb04021950579504907ceecb0daf40ba7f9261
keshav <keshavjha1504@gmail.com>
2022-09-09 19:36:01 +0530

    commit 2

--- render/views.py
+++ render/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'render/index.html', {})
+    return HttpResponse('All Working')
+    # return render(request, 'render/index.html', {})
